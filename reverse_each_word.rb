d9a608f6643c2db900354150890be85fd5d110a0
Blythe Pollard <blytheepollard@gmail.com>
2019-09-10 04:29:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,3 +18,4 @@ end
 # We define as new_array the return value of collect
 # and in its block instead of shoveling into new_array which is what you would do with each
 #you simply collect what you're looking for new_sentence.reverse
+#and then still call join on new_array cause we made it point to the new array created by collect
