d6cfabcad1d91276d7a3f353e17de54df016fac2
Blythe Pollard <blytheepollard@gmail.com>
2019-09-10 04:24:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(sentence)
 end
 new_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  backwards = sentence.split
+   new_array =  backwards.collect do |new_sentence|
+    new_sentence.reverse
+end
+new_array.join(" ")
+end
